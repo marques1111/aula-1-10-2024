24333e25580da05e0b38d4433106b24e4731b329
marques1111 <147104907+marques1111@users.noreply.github.com>
2024-10-01 14:53:50 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'Paris'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Rússia?',
+    'Moscou'
+)
+
+criaCartao(
+    'Lingua Inglesa',
+    'Como se diz Boa Tarde em Inglês?',
+    'Boa tarde em ingles é Good Afternon'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz bom dia em Inglês?',
+    'Bom dia em ingles é Good Morning'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz Tchau em Inglês?',
+    'Tchau em ingles é Bye'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil',
+    'Brasília'
+)
